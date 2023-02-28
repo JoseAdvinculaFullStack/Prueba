a72f391c1e59ad7bd372e27ed7ec48d30a4cb127
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-02-28 13:06:22 -0500

    line decoration

--- src/components/TaskListItem/TaskListItem.js
+++ src/components/TaskListItem/TaskListItem.js
@@ -5,15 +5,21 @@ import Checkmark from '../../images/Checkmark.svg'
 import Checkcancel from '../../images/matt-icons_cancel.svg'
 const BodyItem=(props)=>{
     return(
-        <div className='container-list-item'>
-             {props.text} 
-        </div>
+        <React.Fragment>
+         {props.completed ?
+            <div className={`container-list-item ${props.completed}`}>
+            {props.text} 
+           </div>         
+         : <div className='container-list-item'>
+            {props.text} 
+        </div>}   
+        </React.Fragment>       
     )
 }
 const TaskListItem=(props)=>{
     return (   
        <React.Fragment>
-            <BodyItem text={props.text}></BodyItem>
+            <BodyItem text={props.text} completed={props.completed}></BodyItem>
             <div className='container-buttons'>
             <Buttom image={Checkmark} name="icon" do="checkmark" completed={props.completed} onComplete={props.onComplete}></Buttom>
             <Buttom image={Checkcancel} name="icon" do ="checkcancel"  completed={props.completed} onComplete={props.onComplete}></Buttom>
