57c3b516fd1139f5179e564ab73807f1f9be31a9
JoseAdvinculaFullStack <joseadvinculafullstack@gmail.com>
2023-03-01 07:11:53 -0500

    usando manejo de efectos con useEffect

--- src/components/TaskListContainer/TaskListContainer.js
+++ src/components/TaskListContainer/TaskListContainer.js
@@ -1,44 +1,71 @@
 import Search from '../Search/Search'
 import TaskListItem from '../TaskListItem/TaskListItem'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './TaskListContainer.css'
 import TaskCount from '../TaskCount/TaskCount'
 
-  const defaultTodos=[
-      {text:'Cortar cebolla', completed:false},
-      {text:'Intro a React',completed:false},
-      {text:'Llorar con la llorona',completed:false},
-      {text:'LALALALALA',completed:false}   
-  ]
+//    const defaultTodos=[
+//        {text:'Cortar cebolla', completed:false},
+//        {text:'Intro a React',completed:false},
+//        {text:'Llorar con la llorona',completed:false},
+//        {text:'LALALALALA',completed:false}   
+//    ]
  
 function useLocalStorage(itemName,initialValue){
     
-    const localStorageItem= localStorage.getItem(itemName);
-    let parseItem;
-    if(!localStorageItem){
-        localStorage.setItem(itemName, JSON.stringify(initialValue))
-        parseItem=[];
-    }else{
-        parseItem=JSON.parse(localStorageItem)
+    const [error,setError]=useState(false);
+    const [loading,setLoading]=useState(true);
+    const [item,setItem]=useState(initialValue);
+    useEffect(()=>{
+     setTimeout(()=>{
+        try{
+        const localStorageItem= localStorage.getItem(itemName);
+        let parseItem;
+        if(!localStorageItem){
+            localStorage.setItem(itemName, JSON.stringify(initialValue))
+            parseItem=[];
+        }else{
+            parseItem=JSON.parse(localStorageItem)
+        }
+
+        setItem(parseItem);
+        setLoading(false);
+    }catch(error){
+        setError(error)
     }
-    const [item,setItem]=useState(parseItem);
+     
+     },1000)   
+    })
+    
+    
     const saveItem=(newItem) =>{
+        try{
         const stringifiedItem=JSON.stringify(newItem);
         localStorage.setItem(itemName, stringifiedItem)
         setItem(newItem)
+        } catch{
+            setError(error);
+        }
     }
 
-    return [
+    return {
         item,
-        saveItem
-    ];
+        saveItem,
+        loading,
+        error
+    };
     
 }
 
 
 const TaskListContainer=()=>{
     
-    const [todos,saveTodos] = useLocalStorage('PANA_V2',[]);
+    const {
+        item:todos,
+        saveItem:saveTodos,
+        loading,
+        error
+    } = useLocalStorage('PANA_V2',[]);
 
     const [searchValue,setSearchValue]=useState('');
     
@@ -71,11 +98,16 @@ const TaskListContainer=()=>{
         saveTodos(newTodos);
     };
 
+
     return (
         <div className='task-list-container'>
             <h2>TASK LIST</h2>
             <TaskCount completedTodos={completedTodos} totalTodos={totalTodos}></TaskCount>
             <Search searchValue={searchValue} setSearchValue={setSearchValue}></Search>
+            {error && <p>Desesperate, hubo un erro ...</p>}
+            {loading && <p>Estamos cargando, no desesperes...</p>}
+            {(!loading && !searchTodos.length) && <p>Crea tu primer TODO!</p>}
+            
             {searchTodos.map((item,index) =>(
                 <TaskListItem key={index} text={item.text} completed={item.completed} onComplete={()=>completeTodos(item.text)} onDelete={()=>deleteTodos(item.text)}></TaskListItem>
             ))}
